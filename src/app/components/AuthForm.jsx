9ade2af0f3bd16dd83d38b997e84d9323ac0ebb8
harald-dim <repositorioharald@gmail.com>
2024-06-24 14:21:12 -0300

    ui-sign-in

--- src/app/components/AuthForm.jsx
+++ src/app/components/AuthForm.jsx
@@ -2,10 +2,12 @@
 
 import { useState } from "react";
 
-import React from "react";
-
 export default function AuthForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isNewUser, setIsNewUser] = useState(false);
+  const [isSigningIn, setIsSigningIn] = useState(false);
+  const [isSigningUp, setIsSigningUp] = useState(false);
 
   async function handleLogin(e) {
     e.preventDefault();
@@ -17,9 +19,75 @@ export default function AuthForm() {
     // TASK: handleSingUp
   }
 
+  let signInMessage = "Sign In";
+
+  if (isSigningIn) {
+    signInMessage = "Sign In";
+  } else if (isNewUser) {
+    signInMessage = "Sign Up ";
+  }
+
+  const signInUpMessage = (
+    <p className="text-center text-white">
+      ¡Correo enviado! Revisa tu correo para confirmar la suscripción.
+    </p>
+  );
+
   return (
-    <form onSubit={isNewUser ? handleSingUp : handleLogin}>
-      <input type="text" />
+    <form
+      onSubit={isNewUser ? handleSingUp : handleLogin}
+      className="space-y-8"
+    >
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="email"
+        className="text-zinc-500 appearance-none rounded relative block w-full px-3 py-2 border border-gray-600"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="text-zinc-500 appearance-none rounded relative block w-full px-3 py-2 border border-gray-600"
+        placeholder="password"
+      />
+
+      {/* El type="submit" acciona: <form onSubit={isNewUser ? handleSingUp : handleLogin}>  */}
+      <button
+        type="submit"
+        className="group relative w-full flex justify-center py-2 px-4 border border-transparent"
+      >
+        {signInMessage}
+      </button>
+
+      <p className="text-center text-white">
+        {isNewUser ? (
+          <>
+            ¿Ya tienes una cuenta?
+            <button type="button" onClick={() => setIsNewUser(false)}>
+              iniciar sesión
+            </button>
+          </>
+        ) : (
+          <>
+            <div className="flex justify-center gap-2">
+              <p>¿No tienes una cuenta?</p>
+
+              <button
+                type="button"
+                onClick={() => setIsNewUser(true)}
+                className="text-amber-100 hover:text-amber-200"
+              >
+                <span>¡Crea una cuenta!</span>
+              </button>
+            </div>
+          </>
+        )}
+      </p>
+
+      {/* Si es falso devuelve null, si es verdadero devuelve lo que este en signInUpMessage. */}
+      {isSigningUp && signInUpMessage}
     </form>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
+import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
     <main className="flex items-center min-h-screen flex-col bg-zinc-900 justify-center">
-      <div className="bg-zinc-700 rounded-lg shadow-lg p-6 wfull max-w-lg">
-        <h2 className="text-white text-2xl font-bold mb-4 text-center">Almacenamiento de Fotos</h2>
-        <p className="mb-6 text-lg text-center">Ingrese, suba sus fotos y agregelas a favoritos.</p>
+      <div className="bg-zinc-800/70 rounded-lg shadow-lg p-6 wfull max-w-xl">
+        <h2 className="text-amber-100 text-2xl font-bold mb-4 text-center">
+          Almacenamiento de Fotos
+        </h2>
+        <p className="mb-6 text-lg text-center font-thin">
+          Ingrese, suba sus fotos y agregelas a favoritos.
+        </p>
         {/* TAREA: AuthForm */}
+        <AuthForm />
       </div>
     </main>
   );
